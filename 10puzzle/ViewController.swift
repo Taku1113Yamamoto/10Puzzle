82744593c618dffab81175f37cea5d18e6a0e6f4
taku1113yamamoto <taku.1113.yamamoto@gmail.com>
2022-03-19 19:37:44 +0900

    Initial Commit

---
+++ 10puzzle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  10puzzle
+//
+//  Created by 山本拓 on 2022/03/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
